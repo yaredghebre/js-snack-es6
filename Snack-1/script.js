316f831cfa532c47ad65050d78ea57b9f6abf4c2
yaredghebre <yared2391@gmail.com>
2023-03-24 10:37:14 -0300

    Add: Snack 1 completed

---
+++ Snack-1/script.js
@@ -0,0 +1,43 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const bikes = [
+    {
+        nome: "Garelli",
+        peso: 9
+    },
+    {
+        nome: "Cipollini",
+        peso: 7
+    },
+    {
+        nome: "Lombardo",
+        peso: 11
+    },
+    {
+        nome: "Cinelli",
+        peso: 6
+    },
+    {
+        nome: "Bianchi",
+        peso: 5
+    }
+]
+console.log(bikes);
+
+let lightBike = bikes[0];
+
+for (let i = 0; i < bikes.length; i++) {
+    if(bikes[i].peso < lightBike.peso) {
+        lightBike = bikes[i];
+    }
+};
+
+// DESTRUCTURING e TEMPLATE LITERAL
+const {nome, peso} = lightBike;
+console.log(`La bici più leggera è la ${nome} e il suo peso è ${peso}kg`);
+
+// console.log(lightBike);
+   
---
+++ Snack-Bonus/script.js
@@ -0,0 +1,7 @@
+// Snack Bonus
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
+// es: 
+// const numbersArray = [23, 14, 6, 8, 10];
+// const newArray = getSubArray(numbersArray, 1, 3) ---> [14, 6, 8]
